4b3a56a4f825fc6fe6b786a99af38972ce0e5d06
Austin Rooks <Austionian@users.noreply.github.com>
2024-01-23 10:21:11 -0600

    create and read from remote

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,30 @@
 use anyhow::{Context, Result};
 use axum::{
-    extract::State,
+    extract::{Query, State},
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
+use dotenv::dotenv;
 use mongodb::{bson::doc, options::ClientOptions, Client};
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 use tower_http::trace::TraceLayer;
 
 pub async fn startup() -> Result<Router> {
+    dotenv().ok();
     // Connect to the db.
     // Parse a connection string into an options struct.
-    let mut client_options = ClientOptions::parse("mongodb://localhost:27017")
-        .await
-        .context("Failed to parse mongo client options.")?;
+    let mut client_options = ClientOptions::parse(format!(
+        "mongodb+srv://{}:{}@cluster0.n7uefol.mongodb.net/?retryWrites=true&w=majority",
+        std::env::var("MONGO_USERNAME").context("Failed to get db username")?,
+        std::env::var("MONGO_PASSWORD").context("Failed to get db password")?,
+    ))
+    .await
+    .context("Failed to parse mongo client options.")?;
 
     // Manually set an option, the app name.
     client_options.app_name = Some("My App".to_string());
@@ -36,16 +43,16 @@ pub async fn startup() -> Result<Router> {
         .with_state(client))
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Deserialize, Serialize, Clone)]
 struct Person {
     name: String,
 }
 
 // basic handler that responds with a static string
-async fn root(State(client): State<Client>) -> Result<String, AppError> {
+async fn root(State(client): State<Client>, payload: Query<Person>) -> Result<String, AppError> {
     let db = client.database("test_it_out");
     let collection = db.collection::<Person>("user");
-    let filter = doc! { "name": "Austin Rooks" };
+    let filter = doc! { "name": payload.name.clone() };
     let cursor = collection
         .find_one(filter, None)
         .await
@@ -71,30 +78,27 @@ async fn root(State(client): State<Client>) -> Result<String, AppError> {
 async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
+    payload: Query<Person>,
+    State(client): State<Client>,
+) -> Result<(StatusCode, Json<Person>), AppError> {
     // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
+    let user = Person {
+        name: payload.name.clone(),
     };
+
+    let collection = client.database("test_it_out").collection("user");
+
+    let handle = tokio::task::spawn(async move {
+        collection
+            .insert_one(doc! {"name": payload.name.clone()}, None)
+            .await
+    });
+
+    tokio::time::timeout(Duration::from_secs(5), handle).await???;
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+    Ok((StatusCode::CREATED, Json(user)))
 }
 
 struct AppError(anyhow::Error);
